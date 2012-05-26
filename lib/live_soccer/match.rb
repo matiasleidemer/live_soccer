8f4f951630ecfe467b12aa2c7a7251264aea15df
Matias H. Leidemer <matiasleidemer@gmail.com>
2012-05-26 10:47:58 -0300

    added penalties

--- lib/live_soccer/match.rb
+++ lib/live_soccer/match.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 module LiveSoccer
   class Match
-    attr_accessor :id, :date, :home, :visitor, :score
+    attr_accessor :id, :date, :home, :visitor, :score, :penalty_home, :penalty_visitor
     
     def initialize(attrs)
       self.id      = attrs.fetch :id
@@ -9,11 +9,25 @@ module LiveSoccer
       self.home    = attrs.fetch :home
       self.visitor = attrs.fetch :visitor
       self.score   = attrs.fetch :score
+      self.penalty_home    = attrs.delete(:penalty_home)    || nil
+      self.penalty_visitor = attrs.delete(:penalty_visitor) || nil
     end
     
     def to_s
-      "#{date.strftime("%d%b %H:%M")} - #{self.home} #{self.score} #{self.visitor}"
+      "#{date.strftime("%d%b %H:%M")} - #{self.home} #{score_and_penalties} #{self.visitor}"
     end
     
+    def score_and_penalties
+      with_penalties { self.score }
+    end
+    
+  private
+    def with_penalties
+      "".tap do |output|
+        output << "(#{self.penalty_home}) " if self.penalty_home
+        output << yield
+        output << " (#{self.penalty_visitor})" if self.penalty_visitor
+      end
+    end
   end
 end
--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -4,6 +4,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Match" do
   
   let(:params) { { date: DateTime.parse("20May 18h30"), home: "Vasco", visitor: "Grêmio", score: "1 x 2", id: "40075" } }
+  let(:match)  { LiveSoccer::Match.new params }
   
   describe ".new" do
     it "initializes a new Match class" do
@@ -13,8 +14,13 @@ describe "Match" do
   
   describe "#to_s" do
     it "returns the match as a string" do
-      match = LiveSoccer::Match.new params
       match.to_s.should == "20May 18:30 - Vasco 1 x 2 Grêmio"
+    end
+    
+    it "returns the match with penalties score" do
+      match.penalty_home = "4"
+      match.penalty_visitor = "5"
+      match.to_s.should == "20May 18:30 - Vasco (4) 1 x 2 (5) Grêmio"
     end
   end
   
